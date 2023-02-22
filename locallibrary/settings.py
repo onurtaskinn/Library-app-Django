7e5954f9c565fa2c75034da68c68e2883e0de340
Sammi Chung <20231784@life.hkbu.edu.hk>
2023-02-22 11:45:41 +0100

    fifth commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,14 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9m%wn2lda*3^sqtms@afuv(91atb5!j14lq9kdax+p76gxyuyi'
+#SECRET_KEY = 'django-insecure-9m%wn2lda*3^sqtms@afuv(91atb5!j14lq9kdax+p76gxyuyi'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
-
+#dskfaoidf
 # Application definition
 
 INSTALLED_APPS = [
